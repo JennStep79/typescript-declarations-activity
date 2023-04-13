9b8db8b76aec02b57211548dc58b4386ff76cdc6
Jennifer Stephens <xxjennandtonicxx@gmail.com>
2023-04-12 19:48:04 -0700

    type declarations practice with lodash methods

---
+++ types/index.d.ts
@@ -0,0 +1,9 @@
+declare module 'lodash' {
+
+function multiply(multiplier: number, multiplicand: number): number
+
+function reverse(array: number[]): number[]
+function reverse(array: string[]): string[]
+
+function dropRight(arr:(string|number)[], dropped: number):(string|number)[]
+}
